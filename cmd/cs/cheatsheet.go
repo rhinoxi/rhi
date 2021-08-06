d6a21ba202e0325caff998aaba50ce97319a4f31
rhinoxi <bellanchor@gmail.com>
2021-08-06 12:25:04 +0800

    cheatsheet recursive

--- cmd/cs/cheatsheet.go
+++ cmd/cs/cheatsheet.go
@@ -1,5 +1,10 @@
 package cs
 
-var cheatsheet = map[string]string{
+type csType map[string]interface{}
+
+var csm = csType{
 	"grpc-go": `protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative <path/to/proto>`,
+	"openssl": csType{
+		"generate private key & public certificate": `openssl req -newkey rsa:2048 -nodes -keyout <key.pem> -x509 -days 365 -out <certificate.pem>`,
+	},
 }
--- cmd/cs/command.go
+++ cmd/cs/command.go
@@ -7,9 +7,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const (
+	indentCharacter = "    "
+)
+
 func getCsKeys() []string {
-	keys := make([]string, 0, len(cheatsheet))
-	for key := range cheatsheet {
+	keys := make([]string, 0, len(csm))
+	for key := range csm {
 		keys = append(keys, key)
 	}
 	return keys
@@ -23,6 +27,31 @@ func parenKeys(keys []string) string {
 	return strings.Join(p, " ")
 }
 
+type cmdFormatter struct {
+	sb strings.Builder
+}
+
+func (cf *cmdFormatter) add(key string, value interface{}, indent int) {
+	cf.sb.WriteString(key)
+	cf.sb.WriteString("\n")
+	for i := 0; i <= indent; i++ {
+		cf.sb.WriteString(indentCharacter)
+	}
+	switch tv := value.(type) {
+	case string:
+		cf.sb.WriteString(tv)
+		cf.sb.WriteString("\n")
+	case csType:
+		for k, v := range tv {
+			cf.add(k, v, indent+1)
+		}
+	}
+}
+
+func (cf cmdFormatter) String() string {
+	return cf.sb.String()
+}
+
 func NewCsCmd() *cobra.Command {
 	csKeys := getCsKeys()
 	return &cobra.Command{
@@ -30,19 +59,16 @@ func NewCsCmd() *cobra.Command {
 		Short:                 "show cheatsheet",
 		DisableFlagsInUseLine: true,
 		Run: func(cmd *cobra.Command, args []string) {
-			var sb strings.Builder
+			var cf cmdFormatter
 			if len(args) == 0 {
 				args = csKeys
 			}
 			for _, arg := range args {
-				if line := cheatsheet[arg]; line != "" {
-					sb.WriteString(arg)
-					sb.WriteString("\n\t")
-					sb.WriteString(line)
-					sb.WriteString("\t")
+				if line := csm[arg]; line != nil {
+					cf.add(arg, line, 0)
 				}
 			}
-			fmt.Println(sb.String())
+			fmt.Println(cf)
 		},
 		ValidArgs: csKeys,
 	}
