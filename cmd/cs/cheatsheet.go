f11b89eebe33d9ca40e09798ccb4826759ad6fe3
rhinoxi <bellanchor@gmail.com>
2021-08-31 14:52:50 +0800

    update cs

--- cmd/cs/cheatsheet.go
+++ cmd/cs/cheatsheet.go
@@ -3,7 +3,10 @@ package cs
 type csType map[string]interface{}
 
 var csm = csType{
-	"grpc-go": `protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative <path/to/proto>`,
+	"grpc-go": csType{
+		"new": `protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative <path/to/proto>`,
+		"old": `protoc --go_out=plugins=grpc:. <path/to/proto>`,
+	},
 	"openssl": csType{
 		"generate private key & public certificate": `openssl req -newkey rsa:2048 -nodes -keyout <key.pem> -x509 -days 365 -out <certificate.pem>`,
 	},
